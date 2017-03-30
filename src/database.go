5ebf2051eb4fe137a4afdaa529e144f61c662caf
David Gilhooley <djg5@princeton.edu>
2017-03-29 20:30:12 -0400

    DB: SQL Timestamping
    * Figured out how to get timestamps between dates
      - http://stackoverflow.com/questions/8187288/sql-select-between-dates
    * Golang format parsing also completely blows
      - https://gobyexample.com/time-formatting-parsing
    
    TODO:
     - Get the timestamp modified for between hours

--- src/database.go
+++ src/database.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-    "time"
+    "fmt"
     "log"
+    "math/rand"
+    "time"
     "database/sql"
     _ "github.com/mattn/go-sqlite3"
 )
@@ -77,35 +79,81 @@ func ReadItem(db *sql.DB) []Room {
     }
     return result
 }
+func ReadItemInDateRange(db *sql.DB, start, end time.Time) []Room {
+//    sql_readall := `
+//    SELECT Id, Name, StartTime, EndTime FROM rooms 
+//    WHERE StartTime BETWEEN date('` + start.Format("15:04") + `') AND  date('` +
+//    end.Format("15:04") + `')
+//    ORDER BY datetime(StartTime) DESC
+//    `
+
+    sql_readall := `
+    SELECT Id, Name, StartTime, EndTime FROM rooms 
+    WHERE StartTime BETWEEN "2000-01-02" AND "2000-01-03"
+    `
+    fmt.Println(sql_readall)
+
+    rows, err := db.Query(sql_readall)
+    if err != nil { panic(err) }
+    defer rows.Close()
+
+    var result []Room
+    for rows.Next() {
+        item := Room{}
+        err2 := rows.Scan(&item.Id, &item.Name,
+            &item.StartTime, &item.EndTime)
+        if err2 != nil { panic(err2) }
+        result = append(result, item)
+    }
+    return result
+}
+
+func randDate() [2]time.Time {
+    var t [2]time.Time
+    startDay := rand.Intn(7)
+    startHour := rand.Intn(23)
+
+    t[0] = createDate(startDay, startHour)
+    t[1] = createDate(startDay, startHour + 1)
+    fmt.Println(t[0])
+    return t
+}
+
+func createDate(day, hour int) time.Time {
+    loc, _ := time.LoadLocation("")
+    return time.Date(2000, 1, day, hour, 0, 0, 0, loc)
+}
 
 func main() {
-    const dbpath = "foo.db"
+    // Seed the random generator so we get unique results
+    rand.Seed(time.Now().Unix())
 
+    // Open the Datapath
+    const dbpath = "foo.db"
     db := InitDB(dbpath)
     defer db.Close()
+
+    // Create the DB table
     CreateTable(db)
 
+    // Store 2 items
     items := []Room{
-        Room{"1", "Frist", time.Now(), time.Now()},
-        Room{"2", "Friend", time.Now(), time.Now()},
+        Room{"1", "Frist", createDate(2, 1), createDate(1, 3)},
+        Room{"2", "Friend", createDate(1, 2), createDate(1, 4)},
     }
     StoreItem(db, items)
 
-    readItems := ReadItem(db)
+    // Try and read selection of items
+    readItems :=  ReadItemInDateRange(db, createDate(0,0), createDate(0,5))
     for _, item := range(readItems) {
         log.Printf(item.Name)
     }
+    fmt.Println("Done")
 
-    log.Printf("Next Test")
-
-    items2 := []Room{
-        Room{"1", "Frist", time.Now(), time.Now()},
-        Room{"3", "Sherrard", time.Now(), time.Now()},
-    }
-    StoreItem(db, items2)
-
+    // Read all items
     readItems2 := ReadItem(db)
     for _, item := range(readItems2) {
         log.Printf(item.Name)
+        fmt.Println(item.StartTime)
     }
 }
