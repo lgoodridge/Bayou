90fdbc0229260fb977858939a003fce894e5fee2
Lance Goodridge <ldgoodridge95@gmail.com>
2017-04-01 20:44:34 -0400

    Implemented VectorClock type and respective test
    Fleshed out Bayou Server structure a bit more
    Implemented VectorClock type and methods
    Added VectorClock test to test_test.go

--- src/server.go
+++ src/server.go
@@ -1,31 +1,128 @@
 package bayou
 
+import (
+	"errors"
+	"fmt"
+	"net/rpc"
+	"strings"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
 /************************
  *   TYPE DEFINITIONS   *
  ************************/
 
-/* Go object implemeting a Bayou Server */
+/* Go object representing a Bayou Server */
 type BayouServer struct {
-	id int
+	// Unique index into peers array
+	id	  int
+	// Represents the other bayou servers
+	peers []*rpc.Client
 
-	writeLog []LogEntry
-	undoLog  []LogEntry
+	// Current logical time
+	currTime VectorClock
 
-	ommited    map[int]int
-	timestamps map[int]int
+	// Stores all written ops and their corresponding undo ops
+	WriteLog []LogEntry
+	UndoLog	 []LogEntry
+	// Operations that conflict and fail to merge are stored here
+	ErrorLog []LogEntry
 
-	PersistLog []LogEntry
+	// Maintains ID of latest write discarded from each server
+	Omitted	[]int
 }
+
+/* Vector Clock: parallel array holding a monotonically *
+ * increasing logical time (int) for each peer server   */
+type VectorClock []int
 
 /* Represents an entry in a Bayou server log */
 type LogEntry struct {
+	WriteID	  int
+	Committed bool
+	Timestamp VectorClock
 }
+
+/* Implements sort interface for log entries      *
+ * Sorts by committed vs. tentative, then by time */
+type ByCommitedAndTime []LogEntry
 
 /* AntiEntropy RPC arguments structure */
 type AntiEntropyArgs struct {
-	message string
 }
 
 /* AntiEntropy RPC reply structure */
 type AntiEntropyReply struct {
 }
+
+/* Update or Undo function type:	  *
+ * Takes a database, and returns void */
+type operation func(*sql.DB)
+
+/* Dependency check function type:      *
+ * Takes a database, and returns a bool */
+type depcheck func(*sql.DB) bool
+
+/* Merge process function type:       *
+ * Takes a database, and returns void */
+type mergeproc func(*sql.DB)
+
+/****************************
+ *   BAYOU SERVER METHODS   *
+ ****************************/
+
+/****************************
+ *   VECTOR CLOCK METHODS   *
+ ****************************/
+
+/* Returns a new vector clock of the specified length */
+func NewVectorClock(length int) VectorClock {
+	return make([]int, length)
+}
+
+/* Sets the logical time at idx to specified value        *
+ * Returns an error if newTime is less than current value */
+func (vc VectorClock) SetTime(idx int, newTime int) error {
+	if (newTime < vc[idx]) {
+		return errors.New("SetTime Failed: New time less than current time")
+	}
+	vc[idx] = newTime
+	return nil
+}
+
+/* Increments the logical time at idx */
+func (vc VectorClock) Inc(idx int) {
+	vc[idx] = vc[idx] + 1
+}
+
+/* Returns whether this vector clock is *
+ * strictly "less than" the other one   */
+func (vc VectorClock) LessThan(other VectorClock) bool {
+	if len(vc) != len(other) {
+		return false
+	}
+	// vc is less than other iff each logical time is less
+	// than or equal to the other's logical time for each
+	// peer, and at least one of those is strictly less than
+	strictly_less_seen := false
+	for idx, _ := range vc {
+		if !strictly_less_seen && vc[idx] < other[idx] {
+			strictly_less_seen = true
+		}
+		if (vc[idx] > other[idx]) {
+			return false
+		}
+	}
+	return strictly_less_seen
+}
+
+func (vc VectorClock) String() string {
+	return "VC: " +  strings.Trim(strings.Replace(fmt.Sprint(([]int)(vc)),
+		" ", ", ", -1), "[]")
+}
+
+/***************************
+ *   LOG SORTING METHODS   *
+ ***************************/
+
--- src/test_test.go
+++ src/test_test.go
@@ -2,8 +2,8 @@ package bayou
 
 import (
     "math/rand"
-    "time"
     "testing"
+    "time"
 )
 
 /*************************
@@ -14,10 +14,22 @@ func init() {
     rand.Seed(time.Now().Unix())
 }
 
+/* Fails provided test if condition is not true */
+func assert(t *testing.T, cond bool, message string) {
+    assertEqual(t, cond, true, message)
+}
+
 /* Fails provided test if a and b are not equal */
 func assertEqual(t *testing.T, a interface{}, b interface{}, message string) {
     if a != b {
         t.Fatal(message)
+    }
+}
+
+/* Fails provided test if err is not nil */
+func ensureNoError(t *testing.T, err error, prefix string) {
+    if err != nil {
+        t.Fatal(prefix + err.Error())
     }
 }
 
@@ -50,4 +62,62 @@ func TestDBBasic(t *testing.T) {
         Log.Println(item.Name)
         Log.Println(item.StartTime)
     }
+}
+
+/*****************************
+ *    VECTOR CLOCK TESTS     *
+ *****************************/
+
+/* Fails provided test if VCs are not equal */
+func assertVCEqual(t *testing.T, vc VectorClock, exp VectorClock) {
+    failMsg := "Expected VC: " + exp.String() + "\tReceived: " + vc.String()
+    if len(vc) != len(exp) {
+        t.Fatal(failMsg)
+    }
+    for idx, _ := range vc {
+        if vc[idx] != exp[idx] {
+            t.Fatal(failMsg)
+        }
+    }
+}
+
+/* Unit tests vector clock */
+func TestVectorClock(t *testing.T) {
+    vc := NewVectorClock(4)
+    assertVCEqual(t, vc, VectorClock{0, 0, 0, 0})
+
+    // Ensure Inc works as expected
+    vc.Inc(1)
+    vc.Inc(3)
+    vc.Inc(3)
+    assertVCEqual(t, vc, VectorClock{0, 1, 0, 2})
+
+    // Ensure Set works as expected
+    err := vc.SetTime(0, 6)
+    ensureNoError(t, err, "SetTime returned an error: ")
+    err = vc.SetTime(1, 4)
+    ensureNoError(t, err, "SetTime returned an error: ")
+    err = vc.SetTime(2, 0)
+    ensureNoError(t, err, "SetTime returned an error: ")
+    assertVCEqual(t, vc, VectorClock{6, 4, 0, 2})
+
+    // Ensure Set returns error when trying to
+    // set time less than what is already stored
+    err = vc.SetTime(1, 3)
+    if err == nil {
+        t.Fatal("SetTime did not return an error when rewinding time.")
+    }
+    assertVCEqual(t, vc, VectorClock{6, 4, 0, 2})
+
+    // Ensure LessThan works as expected
+    wrongSize := VectorClock{0, 0, 0}
+    greater := VectorClock{6, 5, 0, 2}
+    equal := VectorClock{6, 4, 0, 2}
+    less := VectorClock{6, 3, 0, 2}
+
+    assert(t, !wrongSize.LessThan(vc), "LessThan returned true for VC of " +
+        "different size")
+    assert(t, !greater.LessThan(vc), "LessThan returned true for greater VC")
+    assert(t, !equal.LessThan(vc), "LessThan returned true for equal VC")
+    assert(t, less.LessThan(vc), "LessThan returned false for lesser VC")
 }
