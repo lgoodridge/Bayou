6e97d2f33a26171f0d1e0f9ec6dc4d5052914883
David Gilhooley <djg5@princeton.edu>
2017-04-19 17:23:36 -0400

    PERSIST: Added beginning persist logic
    - https://gobyexample.com/writing-files
    - http://stackoverflow.com/questions/14121422/de-and-encode-interface-with-gob

--- src/persist.go
+++ src/persist.go
@@ -1,7 +1,31 @@
 package bayou
 
-// TODO: Everything
+import (
+//    "bytes"
+//   "encoding/gob"
+    "io/ioutil"
+//    "os"
+)
+
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func save(data []byte) {
+     err := ioutil.WriteFile("/tmp/bayou-data", data, 0644)
+     check(err)
+//     f, err := os.Create("/tmp/bayou-data")
+//     check(err)
+//     defer f.Close()
 //
-// Probably something along the lines of:
-// save(data []byte)
-// load() []byte
+//     f.Write(data)
+}
+
+func load() ([]byte, error) {
+    dat, err := ioutil.ReadFile("/tmp/bayou-data")
+    // check(err)
+    return dat, err
+}
