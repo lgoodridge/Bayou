0285489ade77f4e23ccb8d5295ae359969bf7890
Lance Goodridge <ldgoodridge95@gmail.com>
2017-04-22 14:52:46 -0400

    Vagrant installs git and go by default

--- vagrant/install-go.sh
+++ vagrant/install-go.sh
@@ -1,11 +1,13 @@
-wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz --progress=bar:force
 sudo tar -C /usr/local -xzf go1.4.2.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+rm -f go1.4.2.linux-amd64.tar.gz
 
-mkdir ~/GoPkgs
-echo 'export GOPATH=~/GoPkgs' >> ~/.bashrc
-
-source ~/.bashrc
+mkdir /home/vagrant/GoPkgs
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=/home/vagrant/GoPkgs
 
 go version
 go get github.com/mattn/go-sqlite3
+
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /home/vagrant/.bashrc
+echo 'export GOPATH=~/GoPkgs' >> /home/vagrant/.bashrc
